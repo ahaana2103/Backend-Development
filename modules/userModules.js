eaea3474622a9a3527c09275857b323f3662f8f6
Ahaana Vatsa <ahaanavatsa@gmail.com>
2026-03-12 15:27:14 +0530

    Adding more users

--- modules/userModules.js
+++ modules/userModules.js
@@ -1,6 +1,11 @@
 const users = [
   { id: 1, name: "Aman" },
-  { id: 2, name: "Riya" }
+  { id: 2, name: "Riya" },
+  { id: 3, name: "Arya" },
+  { id: 4, name: "Subh" },
+  { id: 5, name: "Esha" },
+  { id: 6, name: "Anu" }
+
 ];
 
 module.exports = users;
