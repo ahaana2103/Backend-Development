976ba5190612c9763e2dd3a000c74de99a5b38fe
Ahaana Vatsa <ahaanavatsa@gmail.com>
2026-04-10 12:09:57 +0530

    Updated user modules according to post request

--- modules/userModules.js
+++ modules/userModules.js
@@ -1,11 +1,31 @@
-const users = [
-  { id: 1, name: "Aman" },
-  { id: 2, name: "Riya" },
-  { id: 3, name: "Arya" },
-  { id: 4, name: "Subh" },
-  { id: 5, name: "Esha" },
-  { id: 6, name: "Anu" }
+// const users = [
+//   { id: 1, name: "Aman" },
+//   { id: 2, name: "Riya" },
+//   { id: 3, name: "Arya" },
+//   { id: 4, name: "Subh" },
+//   { id: 5, name: "Esha" },
+//   { id: 6, name: "Anu" }
 
-];
+// ];
 
-module.exports = users;
+// module.exports = users;
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: Number,
+    name: String,
+    age: Number,
+    course: String,
+    skills: [String],
+    address: {
+      city: String,
+      pincode: Number
+    },
+    isActive: Boolean
+  },
+  { collection: "students" } 
+);
+
+module.exports = mongoose.model("User", userSchema);
