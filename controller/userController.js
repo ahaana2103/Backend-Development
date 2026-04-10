88b63b9ed834378d5642e039850ecf5ea1e0bc5d
Ahaana Vatsa <ahaanavatsa@gmail.com>
2026-04-10 12:10:44 +0530

    Updated as per post request

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 // const users = require("../modules/userModules");
 
+//const { db } = require("../modules/userModules")
+
 // const getUsers = (req, res) => {
 //    // res.json(users);
 //    const clientQuery =req.query;
@@ -23,27 +25,61 @@
 
 // module.exports = { getUsers };
 
-const users = require("../modules/userModules");
+// const users = require("../modules/userModules");
 
-const getUsers = (req, res) => {
-   res.json(users);
+// const getUsers = (req, res) => {
+//    res.json(users);
+// };
+
+// const addUser = (req, res) => {
+//    const { id, name } = req.body;
+
+//    if (!id || !name) {
+//       return res.status(400).json({
+//          message: "User data missing"
+//       });
+//    }
+
+//    users.push({ id, name });
+
+//    res.status(201).json({
+//       message: "User created",
+//       user: { id, name }
+//    });
+// };
+
+// module.exports = { getUsers, addUser };
+
+// controllers/userController.js
+
+const User = require("../modules/userModules");
+
+const addUser = async (req, res) => {
+  try { 
+    const { _id, name, age, course, skills, address, isActive }= req.body;
+
+    // check if already exists
+    const existing = await User.findById(_id);
+
+    if (existing) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // create new
+    const user = await User.create({
+      _id,
+      name,
+      age,
+      course,
+      skills,
+      address,
+      isActive
+    });
+
+    res.status(201).json({ message: "User created", user });
+  } catch (err) {
+    res.status(500).json({ message: "Error", error: err.message });
+  }
 };
 
-const addUser = (req, res) => {
-   const { id, name } = req.body;
-
-   if (!id || !name) {
-      return res.status(400).json({
-         message: "User data missing"
-      });
-   }
-
-   users.push({ id, name });
-
-   res.status(201).json({
-      message: "User created",
-      user: { id, name }
-   });
-};
-
-module.exports = { getUsers, addUser };
+module.exports = { addUser };
